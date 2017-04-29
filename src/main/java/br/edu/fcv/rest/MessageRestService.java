dd3a0756f2dd6ed8b5d5b3f5e8a4d19c84d50249
Aluno <aluno@Mac-mini-de-Aluno-2.local>
2017-04-29 13:16:25 -0300

    operações calculadora

--- src/main/java/br/edu/fcv/rest/MessageRestService.java
+++ src/main/java/br/edu/fcv/rest/MessageRestService.java
@@ -15,32 +15,32 @@ public class MessageRestService {
 		return Response.status(200).entity(result).build();
 	}
 	
-//	@GET
-//	@Path("/soma/{param1}/{param2}")
-//	public Response soma(@PathParam("param1") String param1, @PathParam("param2") String param2) {
-//		String result = "Resultado é: " + (Integer.parseInt(param1) + Integer.parseInt(param2));
-//		return Response.status(200).entity(result).build();
-//	}
-//	
-//	@GET
-//	@Path("/subtrai/{param1}/{param2}")
-//	public Response subtrai(@PathParam("param1") String param1, @PathParam("param2") String param2) {
-//		String result = "Resultado é: " + (Integer.parseInt(param1) - Integer.parseInt(param2));
-//		return Response.status(200).entity(result).build();
-//	}
-//	
-//	@GET
-//	@Path("/divide/{param1}/{param2}")
-//	public Response divide(@PathParam("param1") String param1, @PathParam("param2") String param2) {
-//		String result = "Resultado é: " + (Integer.parseInt(param1) / Integer.parseInt(param2));
-//		return Response.status(200).entity(result).build();
-//	}
-//	
-//	@GET
-//	@Path("/multiplica/{param1}/{param2}")
-//	public Response multiplica(@PathParam("param1") String param1, @PathParam("param2") String param2) {
-//		String result = "Resultado é: " + (Integer.parseInt(param1) * Integer.parseInt(param2));
-//		return Response.status(200).entity(result).build();
-//	}
+	@GET
+	@Path("/soma/{param1}/{param2}")
+	public Response soma(@PathParam("param1") String param1, @PathParam("param2") String param2) {
+		String result = "Resultado é: " + (Integer.parseInt(param1) + Integer.parseInt(param2));
+		return Response.status(200).entity(result).build();
+	}
+	
+	@GET
+	@Path("/subtrai/{param1}/{param2}")
+	public Response subtrai(@PathParam("param1") String param1, @PathParam("param2") String param2) {
+		String result = "Resultado é: " + (Integer.parseInt(param1) - Integer.parseInt(param2));
+		return Response.status(200).entity(result).build();
+	}
+	
+	@GET
+	@Path("/divide/{param1}/{param2}")
+	public Response divide(@PathParam("param1") String param1, @PathParam("param2") String param2) {
+		String result = "Resultado é: " + (Integer.parseInt(param1) / Integer.parseInt(param2));
+		return Response.status(200).entity(result).build();
+	}
+	
+	@GET
+	@Path("/multiplica/{param1}/{param2}")
+	public Response multiplica(@PathParam("param1") String param1, @PathParam("param2") String param2) {
+		String result = "Resultado é: " + (Integer.parseInt(param1) * Integer.parseInt(param2));
+		return Response.status(200).entity(result).build();
+	}
 
 }
